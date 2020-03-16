6fc5d4b0ca89a9f07db82a5ebae948bb501a7811
Marcos Paulo <markus@gmail.com>
2020-03-16 11:54:26 -0300

    Primeiro Commit do Projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Projeto Ecommerce</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hello World!<h1>";
+        ?>
+    </body>
+</html>
