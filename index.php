2628944eea060a3226ab6f40ca6e6db937ccfd12
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2020-03-24 19:37:51 -0300

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 
+use app\Core\Core;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/configs/config.inc.php';
+
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 } elseif (session_status() === PHP_SESSION_DISABLED) {
     echo "Error: Verifique as configurações de sessão";
 }
 
-require_once "configs/config.inc.php";
-require_once "vendor/autoload.php";
 
-$core = new \app\Core\Core();
+$core = new Core();
 $core->run();
