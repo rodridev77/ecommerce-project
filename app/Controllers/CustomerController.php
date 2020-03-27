908db1af4333a6828ac0568dc85145bc8bfe8634
marcelinmbb <marcelinobraga@alunos.ifce.edu.br>
2020-03-27 14:35:30 -0300

    Customer Model Atualizado

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -28,17 +28,59 @@ class CustomerController extends Controller{
 		$customer = new Customer();
 		
 		if ($customer->CustomerRegistration($postData)) {
-			$this->data['msg'] = "Cliente cadastrado";
+			echo "Cliente cadastrado";
 		}else{
-			$this->data['msg'] = "Cliente não cadastrado";
+			echo "Cliente nao cadastrado";
 		}
 
-		$viewPath = 'forms/';
+/* 		$viewPath = 'forms/';
 		$viewName = "register_form";
-		$this->loadTemplate($viewPath, $viewName, $this->data);
+		$this->loadTemplate($viewPath, $viewName, $this->data); */
 			
 	}
 
+	public function Update(){
+
+		$data = array("id" => FILTER_VALIDATE_INT
+					,"email"=> FILTER_SANITIZE_STRING,
+					"password_key" => FILTER_SANITIZE_STRING,
+					"name" => FILTER_SANITIZE_STRING, 
+					"surname" => FILTER_SANITIZE_STRING,
+					"phone" => FILTER_SANITIZE_STRING,
+					"cpf" => FILTER_SANITIZE_STRING
+					);
+
+		$postData = filter_input_array(INPUT_POST,$data);
+		
+		$customer = new Customer();
+	
+		if ($customer->CustomerUpdate($postData)) {
+			echo "Cliente atualizado";
+		}else{
+			echo "Cliente nao atualizado";
+		}
+/* 
+		$viewPath = 'forms/';
+		$viewName = "register_form";
+		$this->loadTemplate($viewPath, $viewName, $this->data); */
+			
+	}
+
+	public function Delete(){
+		$id = filter_input(INPUT_POST,"id",FILTER_VALIDATE_INT);
+		$customer = new Customer();
+
+		if($customer->CustomerDelete($id)){
+			echo "cliente deletado";
+		}else{
+			echo "cliente nao deletado";
+		}
+	}
+
+	public function getCustomers(){
+		$customer = new Customer();
+		var_dump($customer->getAllCustomers());
+	}
 }
 
  ?>
